bafcd1ac6e2aaa0e9948940d9b305ae3c769c8f4
Sakina Fakhruddin <spetiwala0@gmail.com>
2020-09-18 12:14:15 -0400

    Update post value

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import models
-
-from fastapi import FastAPI, Request
+import yfinance as yf
+from fastapi import FastAPI, Request, Depends, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from database import SessionLocal, engine
-
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from models import Stock
 
 app = FastAPI()
 
@@ -13,11 +14,43 @@ models.Base.metadata.create_all(bind=engine)
 templates = Jinja2Templates(directory="templates")
 
 
+class StockRequest(BaseModel):
+    symbol: str
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    except Exception as e:
+        print(e)
+    finally:
+        db.close()
+
+
+def fetch_stock_data(id: int):
+    db = SessionLocal()
+    stock = db.query(Stock).filter(Stock.id == id).first()
+    yahoo_data = yf.Ticker(stock.symbol)
+
+    stock.ma200 = yahoo_data.info["twoHundredDayAverage"]
+    stock.ma50 = yahoo_data.info["fiftyDayAverage"]
+    stock.price = yahoo_data.info["previousClose"]
+    stock.forward_pe = yahoo_data.info["forwardPE"]
+    stock.forward_eps = yahoo_data.info["forwardEps"]
+    if yahoo_data.info["dividendYield"]:
+        stock.dividend_yield = yahoo_data.info["dividendYield"] * 100
+
+    db.add(stock)
+    db.commit()
+
+
 @app.get("/")
-def index(request: Request):
+def index(request: Request, db: Session = Depends(get_db)):
     """
     Displays the stock screener dashboard
     """
+    stocks = db.query(Stock).all()
     return templates.TemplateResponse(
         "dashboard.html",
         {
@@ -27,8 +60,20 @@ def index(request: Request):
 
 
 @app.post("/stock")
-def create_stock():
+async def create_stock(
+    stock_request: StockRequest,
+    background_tasks: BackgroundTasks,
+    db: Session = Depends(get_db),
+):
     """
     create_stock - Creates a stock and stores it in the database
     """
-    return {"status": "code_created"}
+    stock = Stock()
+    stock.symbol = stock_request.symbol
+
+    db.add(stock)
+    db.commit()
+
+    background_tasks.add_task(fetch_stock_data, stock.id)
+
+    return {"status": "stock created"}
